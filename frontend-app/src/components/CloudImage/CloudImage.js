af5dd4391511cf21220f7eda7f14fbd99c883771
Sharma, Abhishek <asharma2@conversantmedia.com>
2021-04-23 17:32:02 +0530

    test

--- frontend-app/src/components/CloudImage/CloudImage.js
+++ frontend-app/src/components/CloudImage/CloudImage.js
@@ -1,15 +1,26 @@
-import React from "react";
-import { Image } from "cloudinary-react";
+import React from 'react';
+import { Image } from 'cloudinary-react';
+import { Cloudinary } from 'cloudinary-core';
 
-export const CloudImage = ({ publicId, width, height }) => {
+const cloudinaryCore = new Cloudinary({ cloud_name: 'dp536vvok' });
+
+export const CloudImage = ({ publicId, width, height, className }) => {
+  // return (
+  //   <Image
+  //     cloudName='dp536vvok'
+  //     publicId={publicId}
+  //     width={width}
+  //     height={height}
+  //     crop='scale'
+  //     loading='lazy'
+  //   />
+  // );
   return (
-    <Image
-      cloudName="dp536vvok"
-      publicId={publicId}
+    <img
+      src={cloudinaryCore.url(publicId)}
       width={width}
       height={height}
-      crop="scale"
-      loading="lazy"
+      className={className}
     />
   );
 };
--- frontend-app/src/containers/Home/Home.js
+++ frontend-app/src/containers/Home/Home.js
@@ -1,16 +1,16 @@
-import React, { useEffect } from "react";
-import { addCore } from "../../redux/actions/core";
-import { connect } from "react-redux";
-import ComponentWrapper from "../../components/ComponentWrapper/ComponentWrapper";
-import Banner from "../../components/Banner/Banner";
-import { makeStyles } from "@material-ui/core/styles";
-import Header from "../../components/Header/Header";
-import Footer from "../../components/Footer/Footer";
-import Paper from "@material-ui/core/Paper";
-import Grid from "@material-ui/core/Grid";
-import variables from "../../styles/globalStyles.module.scss";
-import { getCategoryList } from "../../redux/actions/category";
-import { CloudImage } from "../../components/CloudImage/CloudImage";
+import React, { useEffect } from 'react';
+import { addCore } from '../../redux/actions/core';
+import { connect } from 'react-redux';
+import ComponentWrapper from '../../components/ComponentWrapper/ComponentWrapper';
+import Banner from '../../components/Banner/Banner';
+import { makeStyles } from '@material-ui/core/styles';
+import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import variables from '../../styles/globalStyles.module.scss';
+import { getCategoryList } from '../../redux/actions/category';
+import { CloudImage } from '../../components/CloudImage/CloudImage';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -19,38 +19,38 @@ const useStyles = makeStyles((theme) => ({
   },
   paper: {
     backgroundColor: variables.backGroundColor,
-    textTransform: "uppercase",
-    textAlign: "center",
+    textTransform: 'uppercase',
+    textAlign: 'center',
     color: variables.textPrimaryColor,
-    height: "36px",
-    paddingTop: "5px",
+    height: '36px',
+    paddingTop: '5px',
   },
   business: {
-    padding: "3px 1px",
+    padding: '3px 1px',
     // padding: theme.spacing(1),
-    textAlign: "center",
+    textAlign: 'center',
     backgroundColor: variables.backGroundColor,
-    ["@media (min-width:780px)"]: {
+    ['@media (min-width:780px)']: {
       // eslint-disable-line no-useless-computed-key
-      padding: "0px 10px 15px 10px",
+      padding: '0px 10px 15px 10px',
     },
   },
   businessImg: {
-    borderRadius: "5px",
-    width: "100%",
-    height: "150px",
-    border: "1px solid #fff",
+    borderRadius: '5px',
+    width: '100%',
+    height: '150px',
+    border: '1px solid #fff',
     // "max-height": "130px",
-    ["@media (min-width:780px)"]: {
+    ['@media (min-width:780px)']: {
       // eslint-disable-line no-useless-computed-key
-      height: "250px",
+      height: '250px',
     },
-    "&:hover": { transform: "scale3d(1, 1.05, 0.1)" },
+    '&:hover': { transform: 'scale3d(1, 1.05, 0.1)' },
   },
 }));
 
 function Home(props) {
-  console.log(props.categoryList, ".......categoryList");
+  console.log(props.categoryList, '.......categoryList');
   useEffect(() => {
     props.dispatch(getCategoryList({ searchQuery: { parentId: null } }));
     // Update the document title using the browser API
@@ -58,11 +58,11 @@ function Home(props) {
   }, []);
 
   const classes = useStyles();
-  console.log(variables, "hreeeeeeee");
+  console.log(variables, 'hreeeeeeee');
   return (
     <>
       <ComponentWrapper>
-        <Banner props={{ imgPath: "Main Categories/Poster_Babu Banner.jpg" }} />
+        <Banner props={{ imgPath: 'Main Categories/Poster_Babu Banner.jpg' }} />
         <Grid container className={classes.root}>
           <Grid item xs={12} className={classes.paper}>
             <strong>Select your business</strong>
@@ -71,9 +71,10 @@ function Home(props) {
             <Grid item xs={12} sm={6}>
               <div className={classes.business}>
                 <CloudImage
-                  publicId={img.file.public_id}
-                  height="150px"
-                  width="100%"
+                  publicId={img.url}
+                  height='150'
+                  width='100%'
+                  className={classes.businessImg}
                 />
                 {/* <img
                   src={`Main Categories/${img}`}
